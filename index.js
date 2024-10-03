68e46d04737adb6aed13f12b1be637dd6e2bb9cb
technical-trangle <trangle20896@gmail.com>
2024-10-03 16:15:19 +0200

    UPDATE

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+// const audio = document.getElementById('audioPlayer');
+// const playPauseBtn = document.getElementById('playPauseBtn');
+
+// playPauseBtn.addEventListener('click', function () {
+//   if (audio.paused) {
+//     audio.play();
+//     playPauseBtn.textContent = 'Pause';
+//   } else {
+//     audio.pause();
+//     playPauseBtn.textContent = 'Play';
+//   }
+// });
+
+
 const audio = document.getElementById('audioPlayer');
 const playPauseBtn = document.getElementById('playPauseBtn');
 
+// Check if the audio element is ready and ensure mobile browsers allow user interaction
 playPauseBtn.addEventListener('click', function () {
   if (audio.paused) {
     audio.play();
@@ -10,7 +25,4 @@ playPauseBtn.addEventListener('click', function () {
     playPauseBtn.textContent = 'Play';
   }
 });
-
-
-
 
