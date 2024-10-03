438a35555bdf183a37a03c5463fe4fe2ded13994
technical-trangle <trangle20896@gmail.com>
2024-10-03 18:23:13 +0200

    update

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-// const audio = document.getElementById('audioPlayer');
-// const playPauseBtn = document.getElementById('playPauseBtn');
 
-// playPauseBtn.addEventListener('click', function () {
-//   if (audio.paused) {
-//     audio.play();
-//     playPauseBtn.textContent = 'Pause';
-//   } else {
-//     audio.pause();
-//     playPauseBtn.textContent = 'Play';
-//   }
-// });
-
-// SECOND TROUBLE SHOOT
 const audio = document.getElementById('audioPlayer');
 const playPauseBtn = document.getElementById('playPauseBtn');
 
