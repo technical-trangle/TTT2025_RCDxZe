0c0b86380279a4e515f40a022aad8000d8a4f24f
technical-trangle <trangle20896@gmail.com>
2024-10-03 15:51:13 +0200

    update

---
+++ index.js
@@ -0,0 +1,12 @@
+const audio = document.getElementById('audioPlayer');
+const playPauseBtn = document.getElementById('playPauseBtn');
+
+playPauseBtn.addEventListener('click', function () {
+  if (audio.paused) {
+    audio.play();
+    playPauseBtn.textContent = 'Pause';
+  } else {
+    audio.pause();
+    playPauseBtn.textContent = 'Play';
+  }
+});
